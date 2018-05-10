099d9a8b64d82ea4a72616e77a2bf942fe21194d
DryFear <voronin8945@mail.ru>
2018-05-10 17:32:06 +0300

    Added psvm and created frame

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,18 @@
-public class Main {
+import java.awt.*;
+
+public class Main extends Frame{
+
+
+    private Main(){
+        setResizable(false);
+        setVisible(true);
+        setSize(300, 400);
+
+    }
+
+
+    public static void main(String[] args) {
+        new Main();
+
+    }
 }
